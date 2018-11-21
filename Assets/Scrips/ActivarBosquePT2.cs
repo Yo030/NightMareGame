407d3a0d2166a4862de2317d3ec3e2d5aa7ba433
Yo030 <seryrak49@gmail.com>
2018-11-21 14:17:54 -0600

    Este si es el Proyecto

---
+++ Assets/Scrips/ActivarBosquePT2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarBosquePT2 : MonoBehaviour {
+
+    public GameObject PT1;
+    public GameObject PT2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ActivarBosquept2")
+        {
+            PT1.SetActive(false);
+            PT2.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scrips/TextoApareceEnPantalla.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoApareceEnPantalla : MonoBehaviour {
+
+    public GameObject Texto;
+    public GameObject Fire;
+    public GameObject Self;
+    public GameObject New;
+    bool inTrigger = false;
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump") && inTrigger == true)
+        {
+            Self.SetActive(false);
+            New.SetActive(true);
+        }
+
+
+
+    }
+
+    void LateUpdate()
+    {
+        if (Self.activeSelf == false)
+        {
+            Texto.SetActive(false);
+            inTrigger = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Vela")
+        {
+            Texto.SetActive(true);
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Vela")
+        {
+            Texto.SetActive(false);
+            inTrigger = false;
+        }
+    }
+}
